0a1f0e8fe4a3afdf5e9b18d60507735da6044299
lleticiasilvaa <leticia.silva1@ufv.br>
2022-11-03 19:36:29 -0300

    gerando caminho que só desce

--- executaveis/lerArquivo.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include "matriz.h"
-
-FILE *arquivoDeEntrada;
-int lerArquivoPrincipal(char **nomeArquivo, int *l, int *c, Celula ***matriz);
--- makefile
+++
@@ -1,7 +0,0 @@
-
-all: 
-	gcc main.c ./executaveis/backtracking.c ./executaveis/lerArquivo.c ./executaveis/matriz.c ./executaveis/pilha.c -o main -Wall -lm 
-	./main
-	
-clean:
-	rm ./bin/* ./obj/*
