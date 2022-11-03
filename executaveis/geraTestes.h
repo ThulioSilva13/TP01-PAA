30ca3f1d30d4c8946a35553f6e7dcd9c673ce94d
ThulioSilva13 <thuliomarcus2001@gmail.com>
2022-11-03 20:37:23 -0300

    add gerarArquivo

---
+++ executaveis/geraTestes.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "matriz.h"
+
+void preencheAleatoriamente(int **matriz, int N, int M);
+void preencheCaminho(int **matriz, int N, int M);
